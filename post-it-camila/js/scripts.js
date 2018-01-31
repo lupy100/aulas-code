f5c5a1deb0296de466be4f868c819ae01f1e0e67
João Matheus <joao.matheus2819@gmail.com>
2018-01-31 21:55:17 -0200

    babel install

--- post-it-camila/js/scripts.js
+++ post-it-camila/js/scripts.js
@@ -1,75 +1,93 @@
 //array de notas/ variavel que representa a nota
-var notas = []
-
+var notas = [];
+// const calcularAreaCirculo = (raio) => {
+// 	const PI = 3.14;
+// 	return PI * raio * raio;
+// };
 var listaNotas = {
   secao: document.getElementsByClassName("notes")[0],
   listaInterna: [],
-  adiciona: function (titulo, texto) {
+  adiciona: (titulo, texto) => {
     var nota = {
       titulo: titulo,
       texto: texto,
       editando: false
     };
-    this.listaInterna.push(nota);
-    atualizarSecao(this.secao);
+    listaNotas.listaInterna.push(nota);
+    atualizarSecao(listaNotas.secao);
   },
-  remove: function (index) {
-    this.listaInterna.splice(index, 1);
-    atualizarSecao(this.secao);
+  remove: (index) => {
+    listaNotas.listaInterna.splice(index, 1);
+    atualizarSecao(listaNotas.secao);
   },
-  edita: function (index) {
-    this.listaInterna[index].editando = true;
-    atualizarSecao(this.secao);
+  edita: (index) => {
+    listaNotas.listaInterna[index].editando = true;
+    atualizarSecao(listaNotas.secao);
   },
-  salva: function (index, novoTitulo, novoTexto) {
-    this.listaInterna[index].titulo = novoTitulo;
-    this.listaInterna[index].texto = novoTexto;
-    this.listaInterna[index].editando = false;
-    atualizarSecao(this.secao);
+  salva: (index, novoTitulo, novoTexto) => {
+    listaNotas.listaInterna[index].titulo = novoTitulo;
+    listaNotas.listaInterna[index].texto = novoTexto;
+    listaNotas.listaInterna[index].editando = false;
+    atualizarSecao(listaNotas.secao);
+  },
+  pegaNota: (index) => {
+    return listaNotas.listaInterna[index];
+  },
+  contaItem: () => {
+    return listaNotas.listaInterna.length;
   }
-}
-// Lista 
-function atualizarSecao(secao) {
+
+  // Lista
+};
+
+const atualizarSecao = (secao) => {
   //criar uma variavel que guarda o html de todas as notas que deven aparecer na tela
   var innerHTML = "";
   //percorerre a lista de notas e criar o template de cada nota, e colocar na viriavel
-  for (var i = 0; i < listaNotas.listaInterna.length; i++) {
-    if (listaNotas.listaInterna[i].editando) {
-      innerHTML += '<form class="note">' +
-        '<input class="note__title" type="text" name="title" value="' + listaNotas.listaInterna[i].titulo + '" placeholder="Título" />' +
-        '<textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">' + listaNotas.listaInterna[i].texto + '</textarea>' +
-        '<button class="note__control" type="button" onclick="adicionarNota(this.form.title, this.form.texto,this.form,this.form.parentElement, ' + i + ')">' +
-        'Concluído' +
-        '</button>' +
-        '</form>';
+  for (var i = 0; i < listaNotas.contaItem(); i++) {
+    var notaAtual = listaNotas.pegaNota(i);
+    if (notaAtual.editando) {
+      innerHTML +=
+        `<form class="note">
+        <input class="note__title" type="text" name="title" value="${notaAtual.titulo}" placeholder="Título" />
+        <textarea class="note__body" name="texto" rows="5" placeholder="Criar uma nota...">
+          ${notaAtual.texto}
+        </textarea> 
+        <button class="note__control" type="button" onclick="adicionarNota(listaNotas.form.title, listaNotas.form.texto,listaNotas.form,${i})">
+        Concluído 
+        </button>
+        </form>`;
     } else {
-      innerHTML += '<form class="note" onclick="editarFormulario(' + i + ')">' +
-        '<button class="note__control" type="button" onclick="removerNota(' + i + ',event)">' +
-        '<i class="fa fa-times" aria-hidden="true"></i>' +
-        '</button>' +
-        '<h1 class="note__title">' + listaNotas.listaInterna[i].titulo + '</h1>' +
-        '<p class="note__body">' + listaNotas.listaInterna[i].texto + '</p>' +
-        '</form>';
+      innerHTML +=
+      `<form class="note" onclick="editarFormulario(${i})">
+        <button class="note__control" type="button" onclick="removerNota(${i},event)">
+        <i class="fa fa-times" aria-hidden="true"></i>
+        </button>
+        <h1 class="note__title">
+        ${notaAtual.titulo}
+        </h1>
+        <p class="note__body">
+        ${notaAtual.texto} 
+        </p>
+        </form>`;
     }
   }
   //colocar o html de todo mundo dentro (inner) da secao
   secao.innerHTML = innerHTML;
 }
 
-function adicionarNota(inputTitle, inputText, formulario, secao, index) {
-  if (listaNotas.listaInterna[index]) {
+const adicionarNota = (inputTitle, inputText, formulario, index) => {
+  if (listaNotas.pegaNota(index)) {
     listaNotas.salva(index, inputTitle.value, inputText.value);
   } else {
     listaNotas.adiciona(inputTitle.value, inputText.value);
-    formulario.reset()
+    formulario.reset();
   }
 }
 
-function removerNota(indice, event) {
+const removerNota = (indice, event) => {
   event.stopPropagation();
   listaNotas.remove(indice);
 }
 
-function editarFormulario(indice) {
-  listaNotas.edita(indice);
-}
+const editarFormulario = (indice) => listaNotas.edita(indice);
