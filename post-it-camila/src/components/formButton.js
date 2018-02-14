7a2aba159a28a92d965b16d98b3b282ecefb1f4f
João Matheus <joao.matheus2819@gmail.com>
2018-02-14 19:38:58 -0200

    install react

--- post-it-camila/src/components/formButton.js
+++ post-it-camila/src/components/formButton.js
@@ -1,16 +1,8 @@
-// props param
-function FormButton(props) {
-    let formButton = document.createElement('button');
-    
-    // destructuring
-    formButton.setAttribute('class', props.className);
-    formButton.setAttribute('type', props.type);
-    
-    formButton.addEventListener('click', props.click);
+import React from 'react'
 
-    formButton.innerHTML = props.children;
-
-    return formButton;
+//tipo 1 = sem arrow 
+function FormButton(props,children) {
+    return React.createElement("button",props,children)
 }
 
 export default FormButton;
