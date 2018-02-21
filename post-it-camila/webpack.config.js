13545c449c0dd94bbdc484d03fd21ef512fba397
viniosouza <vinicios2508@gmail.com>
2018-02-21 20:51:37 -0300

    Webpack install

--- post-it-camila/webpack.config.js
+++ post-it-camila/webpack.config.js
@@ -1,19 +1,19 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            
-             }
-        ]
-    }
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        use: ["style-loader", "css-loader", 'file-loader']
+      }
+    ]
+  }
 };
